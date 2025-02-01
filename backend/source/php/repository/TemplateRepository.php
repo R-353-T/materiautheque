340997d215916d16a74d2d2a559284e2baf494a1
Adrien Villalonga <nd4yza9cgmdx4cxpq4box6tv7nh80zgc@proton.me>
2025-02-01 15:17:01 +0100

    feature: load group fieldList during template list

--- backend/source/php/repository/TemplateRepository.php
+++ backend/source/php/repository/TemplateRepository.php
@@ -11,11 +11,13 @@ class TemplateRepository extends Repository
     protected string $table = "mate_template";
     protected string $model = TemplateModel::class;
     private readonly GroupRepository $groupRepository;
+    private readonly FieldRepository $fieldRepository;
 
     public function __construct()
     {
         parent::__construct();
         $this->groupRepository = GroupRepository::inject();
+        $this->fieldRepository = FieldRepository::inject();
     }
 
     public function update($model): ?object
@@ -37,6 +39,7 @@ class TemplateRepository extends Repository
             $model->childGroupList = $this->groupRepository->selectTemplateChildList($model->id);
 
             foreach ($model->childGroupList as $childGroup) {
+                $childGroup->fieldList = $this->fieldRepository->selectByGroupId($childGroup->id);
                 $this->treeGroupChildList($childGroup);
             }
         }
@@ -49,6 +52,7 @@ class TemplateRepository extends Repository
         $group->childGroupList = $this->groupRepository->selectGroupChildList($group->id);
 
         foreach ($group->childGroupList as $childGroup) {
+            $childGroup->fieldList = $this->fieldRepository->selectByGroupId($childGroup->id);
             $this->treeGroupChildList($childGroup);
         }
     }
